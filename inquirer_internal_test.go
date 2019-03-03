bebef19aa50af2e0e6cb9f8f7c96673c933d3914
Shadowbeetle <tamas.kadlecsik@gmail.com>
2019-03-03 21:19:01 +0100

    placholder for internal function tests

---
+++ inquirer_internal_test.go
@@ -0,0 +1,15 @@
+package inquirer
+
+import "testing"
+
+func TestSetDefaultsFull(t *testing.T) {
+
+}
+
+func TestDefaultsEmpty(t *testing.T) {
+
+}
+
+func TestDefaultsWithQuestion(t *testing.T) {
+
+}

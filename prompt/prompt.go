29099532b8a8bb786c78f2f00b1b69402a3d70a1
Shadowbeetle <tamas.kadlecsik@gmail.com>
2019-03-05 17:31:44 +0100

    fix doc comment on package

--- prompt/prompt.go
+++ prompt/prompt.go
@@ -1,4 +1,4 @@
-// Package simple-prompt exposes provides a simple prompt for user input in CLI applications.
+// Package prompt provides a simple prompt for user input in CLI applications.
 // while there are other fantastic packages that provide such functionality eg. as https://github.com/c-bata/go-prompt
 // or https://github.com/AlecAivazis/survey most of them provide functionality one might not need while hacking together simple CLI appplications
 // simple prompt aims to be quick to understand and use, or to just provide an example you can copy and paste from the parts necessary for your use-case.
